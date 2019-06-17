0bfdcfc0cfaec39f0ee4e731ab38e4d21b38e987
Manjunath <mkambale@teksystems.com>
2019-06-17 19:24:07 +0530

    added teat case

--- TestUnitTestApp/TestUnitTestAppTests/DetailsTableViewControllerTests.swift
+++ TestUnitTestApp/TestUnitTestAppTests/DetailsTableViewControllerTests.swift
@@ -1,1 +1,46 @@
+//
+//  DetailsTableViewControllerTests.swift
+//  DetailsTableViewControllerTests
+//
+//  Created by Manjunath K on 6/4/19.
+//  Copyright © 2019 teksystems. All rights reserved.
+//
 
+import XCTest
+@testable import TestUnitTestApp
+
+class DetailsTableViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+    func testsomeFuncToTestPass() {
+        let viewC = DetailsTableViewController()
+        
+        let retValue = viewC.someFunc(someVal: true)
+        
+        XCTAssert(retValue == false, "testsomeFuncToTestPass")
+    }
+    
+    func testsomeFuncToTestFail() {
+        let viewC = DetailsTableViewController()
+        
+        let retValue = viewC.someFunc(someVal: false)
+        
+        XCTAssert(retValue == true, "testsomeFuncToTestFail")
+    }
+    
+    func testsome1() {
+        let viewC = DetailsTableViewController()
+        
+        viewC.some1()
+    }
+
+
+}
